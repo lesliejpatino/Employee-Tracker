8b84f4e136a4345bfe2acadd4c3b4d8def34b90b
Leslie Patino <lesliejpatino@gmail.com>
2022-02-01 18:22:37 -0800

    created seeds.sql file that will insert values into the department, role, and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -9,15 +9,19 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-
+    salary DECIMAL,
+    department_id INT, -- will hold reference to dept role
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
+    role_id INT, -- will hold reference to employee role
+    manager_id INT -- will hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,41 @@
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Employee Wellness"),
+        ("Finance"),
+        ("Creative Solutions"),
+        ("Consumer Products");
+
+INSERT INTO role (title, salary)
+VALUES  ("Web Developer", 110000),
+        ("UI/UX Designer", 100000),
+        ("Engineering Manager", 150000)
+        ("Chief Happiness Officer", 80000),
+        ("People and Culture Officer", 80000),
+        ("Employee Wellness Manager", 100000)
+        ("Budget Analyst", 80000),
+        ("Finance Advisor", 90000),
+        ("Finance Manager", 140000)
+        ("Marketing Specialist", 60000),
+        ("Brand Strategist", 60000),
+        ("Creative Solutions Manager",80000)
+        ("Buyer", 60000),
+        ("Production Specialist", 50000),
+        ("Consumer Products Manager", 70000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES  ("Mila","Ross"),
+        ("Rick","Morty"),
+        ("Drew","Lee"),
+        ("Tamara","Robbins"),
+        ("Bella","Roberts"),
+        ("Oscar","Gomez"),
+        ("Ramon","DeLuca"),
+        ("Nicole","Ramos"),
+        ("Jinn",""),
+        ("Asher","Mortensen"),
+        ("Todd","Sharp"),
+        ("Emma","Walton"),
+        ("Francesca","Stine"),
+        ("Rose","Edwards"),
+        ("Tony","Peck");
+
